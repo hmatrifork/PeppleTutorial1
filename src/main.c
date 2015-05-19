72fe63a850031f091fe2244bc063ea551a61eff9
Henrik Madsen <hma@trifork.com>
2015-05-19 22:07:34 +0200

    Tutorial 2

--- src/main.c
+++ src/main.c
@@ -2,6 +2,9 @@
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static GFont s_time_font;
+static BitmapLayer *s_background_layer;
+static GBitmap *s_background_bitmap;
 
 static void update_time() {
   // Get a tm structure
@@ -29,13 +32,22 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 }
 
 static void main_window_load(Window *window) {
+  // Create GBitmap, then set to created BitmapLayer
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
+  s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
+  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
+
   // Create time TextLayer
-  s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
+  s_time_layer = text_layer_create(GRect(5, 52, 139, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
 
-  // Improve the layout to be more like a watchface
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  // Create GFont
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_DOS_48));
+
+  // Apply to TextLayer
+  text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
   // Add it as a child layer to the Window's root layer
@@ -43,8 +55,14 @@ static void main_window_load(Window *window) {
 }
 
 static void main_window_unload(Window *window) {
-    // Destroy TextLayer
-    text_layer_destroy(s_time_layer);
+  // Destroy TextLayer
+  text_layer_destroy(s_time_layer);
+  // Unload GFont
+  fonts_unload_custom_font(s_time_font);
+  // Destroy GBitmap
+  gbitmap_destroy(s_background_bitmap);
+  // Destroy BitmapLayer
+  bitmap_layer_destroy(s_background_layer);
 }
 
 static void init() {
